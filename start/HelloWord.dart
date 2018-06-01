a4f387c3d01addcead43f32a77e64528baf85c6f
Iamsdt <shudiptotrafder@gmail.com>
2018-06-01 17:47:38 +0600

    Initial commit

---
+++ start/HelloWord.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+//comment
+/*multi line comment
+*/
+
+void main() {
+  print("Hello word");
+
+  int i = 10;
+  printInteger(i);
+
+  //variables
+  var v = 10; //this can hold any type of data
+
+  dynamic s = "s";
+  String ds = "Ds"; //explicitly
+  String sin = 's'; //single quote
+  String df = 'Shudipto';
+
+  //set default value
+  int lineCount;
+  assert(lineCount == null);
+
+  //final and constant
+
+}
+
+printInteger(int i) {
+  print(i);
+}
