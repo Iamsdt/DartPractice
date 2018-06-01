7107a7fc8c328f48dbf797a04e585b7ec1ddbc03
Iamsdt <shudiptotrafder@gmail.com>
2018-06-01 19:11:30 +0600

    implemented later

---
+++ start/CollectionExample.dart
@@ -0,0 +1,6 @@
+/*
+ * Created by Shudipto Tarfder
+ * on 6/1/18 7:10 PM
+ * Copyright (c)Shudipto Tarfder.
+ */
+
---
+++ start/StringExample.dart
@@ -0,0 +1,12 @@
+/*
+ * Created by Shudipto Tarfder
+ * on 6/1/18 7:10 PM
+ * Copyright (c)Shudipto Tarfder.
+ */
+
+void main(){
+
+  //reverse a string
+  var input = "Bangladesh";
+  print(input.split('').reversed.join());
+}
