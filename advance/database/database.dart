6fa8c6f7a35ce91ddb1b0eecdac149378f80bd04
Iamsdt <shudiptotrafder@gmail.com>
2018-06-30 17:18:05 +0600

    database
    Took 27 minutes

---
+++ advance/database/database.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:sqljocky5/constants.dart';
+import 'package:sqljocky5/sqljocky.dart';
+import 'package:sqljocky5/utils.dart';
+
+void main() async{
+
+  var pool = new ConnectionPool(
+      host: 'localhost',
+      port: 3306,
+      user: 'bryan',
+      password: 'password',
+      db: 'school',
+      max: 5
+  );
+
+  var results = await pool.query('Select * from teachers');
+
+  print('Results ${await results.length} rows');
+
+  pool.closeConnectionsNow();
+  exit(0);
+
+}
---
+++ advance/socket/UDP_SOCKET.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'dart:convert';
+
+main(List<String> arguments) {
+
+  var data = 'Hello World';
+
+  // ignore: deprecated_member_use
+  List<int> dataToSend = UTF8.encode(data);
+  int port = 3000;
+
+  //Server
+  // ignore: deprecated_member_use
+  RawDatagramSocket.bind(InternetAddress.LOOPBACK_IP_V4, port).then((RawDatagramSocket udpSocket){
+    udpSocket.listen((RawSocketEvent event) {
+      if(event == RawSocketEvent.READ) {
+        Datagram dg = udpSocket.receive();
+        print(UTF8.decode(dg.data));
+      }
+    });
+
+
+    //client
+    udpSocket.send(dataToSend, InternetAddress.LOOPBACK_IP_V4, port);
+    print('Sent!');
+  });
+}
